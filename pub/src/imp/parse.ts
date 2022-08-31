ee3ed511b117e7d4babca598a06aa92a90f1da0f
corno <corno.astn.a@schraverus.com>
2022-08-31 12:14:54 +0200

    .

--- pub/src/imp/parse.ts
+++ pub/src/imp/parse.ts
@@ -9,45 +9,49 @@ export const parse: api.Parse = ($, $i, $d) => {
             tsconfigPath: $.tsConfigPath
         },
         {
-            callback: ($) => {
-                $.files.forEach((a, b) => a > b, ($, key) => {
-                    x.parse(
-                        $.root,
-                        {
-                            reportMissingToken: ($) => {
-                                $i.reportMissingToken(
-                                    {
-                                        parentAnnotation: $.parentAnnotation,
-                                        path: $.path,
-                                        kindNameOptions: $.kindNameOptions
-                                    }
-                                )
+            onFile: ($$) => {
+                const $ = $$.data
+                const key = $$.path
 
-                            },
-                            reportUnexpectedToken: ($) => {
-                                $i.reportUnexpectedToken(
-                                    {
-                                        path: $.path,
-                                        token: $.token.implementationDetails,
-                                        expected: $.expected,
-                                    }
-                                )
+                x.parse(
+                    $.root,
+                    {
+                        reportMissingToken: ($) => {
+                            $i.reportMissingToken(
+                                {
+                                    parentAnnotation: $.parentAnnotation,
+                                    path: $.path,
+                                    kindNameOptions: $.kindNameOptions
+                                }
+                            )
 
-                            },
-                            callback: ($) => {
-                                $i.onFile({
-                                    path: key,
-                                    data: $,
-                                })
-                            },
                         },
-                        {
-                            doUntil: $d.doUntil,
-                            lookAhead: $d.lookAhead,
-                        }
-                    )
-                })
-            }
+                        reportUnexpectedToken: ($) => {
+                            $i.reportUnexpectedToken(
+                                {
+                                    path: $.path,
+                                    token: $.token.implementationDetails,
+                                    expected: $.expected,
+                                }
+                            )
+
+                        },
+                        callback: ($) => {
+                            $i.onFile({
+                                path: key,
+                                data: $,
+                            })
+                        },
+                    },
+                    {
+                        doUntil: $d.doUntil,
+                        lookAhead: $d.lookAhead,
+                    }
+                )
+            },
+            onEnd: () => {
+
+            },
 
         }
     )
