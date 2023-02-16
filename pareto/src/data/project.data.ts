db64b3d5470a41be71fb2f73ede051e096072bb5
corno <corno.astn.a@schraverus.com>
2023-02-16 19:47:27 +0100

    .

--- pareto/src/data/project.data.ts
+++ pareto/src/data/project.data.ts
@@ -11,7 +11,7 @@ export const $: mproject.T.Project = {
     'description': "parse typescript source code into a typesafe AST",
     'license': "ISC",
 
-    'pubdependencies': d({
+    'dependencies': d({
     }),
     'type': ['library', {
         'main': {
@@ -23,6 +23,13 @@ export const $: mproject.T.Project = {
         'test': {
             'dependencies': d({
             }),
+            'glossary': {
+                'functions': d({}),
+                'imports': d({}),
+                'parameters': d({}),
+                'types': d({}),
+                'interfaces': d({}),
+            },
         }
     }],
 }
--- pub/src/interface/dependencies/dependencies.p.ts
+++
@@ -1,7 +0,0 @@
-import * as pm from 'pareto-core-state'
-
-export type DDependencies = {
-    //doUntil: <T>(stack: pm.Stack<T>, callback: ($: T) => boolean) => void,
-    lookAhead: <T>(stack: pm.Stack<T>, exists: ($: T) => void, notExists: () => void) => void,
-    stringsAreEqual: (a: string, b: string) => boolean,
-}
